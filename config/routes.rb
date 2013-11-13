605781490d12bd0bc2b5a294ce45c44e91021302
Alex Abreu <alexabreu@gmail.com>
2013-11-13 15:21:41 -0700

    View added for editing of user profile.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Snapp::Application.routes.draw do
   
   devise_for :users
-  resources :users do
+  
+  resources :users, only: [:index] do
     resources :posts do
       resources :comments
       resources :likes
@@ -33,7 +34,7 @@ Snapp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'welcome#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
